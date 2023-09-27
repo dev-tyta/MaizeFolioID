73b4839d6099dc9d5063f93efd0bf22cd8a34e40
Testimony Adekoya <74291997+dev-tyta@users.noreply.github.com>
2023-09-27 08:48:38 +0100

    Deploying app to github

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import time
+from .models.load_model import download_model
+from .data.preprocess_img import preprocess
+from .models.predict_model import prediction, classifier
+
+
+model = download_model("Testys/MaizeFolioID", "model_2.h5")
+
+
+def run():
+    st.title("MaizeFolioID")
+    st.write("Introducing 'MaizeFolioID': This application applies an advanced "
+             "image recognition technology to accurately classify and identify various foliar "
+             "diseases affecting maize leaves. " 
+             "Simply capture a photo of the leaf, and let MaizeFolioID analyze and detect type in seconds, "
+             "helping farmers make informed decisions for healthier crop management.")
+    img_in = st.file_uploader(label="Upload the photo of your maize leaf here.", type=["png", "jpg", "jpeg"])
+    if img_in is not None:
+        img_content = img_in.read()
+        processed_img = preprocess(img_content, target_shape=(224, 224))
+        img_pred = prediction(model, x=processed_img)
+        class_name = classifier(img_pred)
+    
+        with st.spinner(text="Detecting Diseases..."):
+            time.sleep(10)
+        st.write(f"The uploaded maize leaf belongs to the {class_name} class.")
+
+
+if __name__ == "__main__":
+    run()
