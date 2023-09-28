e82b2f375f81439b54723e7eb7791cd7c81b4a1e
Testimony Adekoya <74291997+dev-tyta@users.noreply.github.com>
2023-09-28 18:25:13 +0100

    Changing name

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -1,10 +1,1 @@
-from huggingface_hub import hf_hub_download
-from tensorflow.keras.models import load_model
 
-
-def download_model(repo, file):
-    repo = repo  # Hugging Face Repository - "Testys/MaizeFolioID"
-    file = file  # File within Hugging Face repository "model_2.h5"
-
-    model_2 = load_model(hf_hub_download(repo_id=repo, filename=file))
-    return model_2
