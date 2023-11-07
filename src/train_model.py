af9d86e8a236d2efc0558f50dbab301a5cc07dfc
Testimony Adekoya <hardeykoryar7@gmail.com>
2023-11-07 13:35:36 +0100

    Updating FDD notebook

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import splitfolders
+import python_splitter
+import matplotlib.pyplot as plt
 from tensorflow.keras.applications.vgg16 import VGG16,preprocess_input
+from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras import layers, models, losses
+from tensorflow.keras import losses
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.models import Model, Sequential, load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from huggingface_hub import hf_hub_download
+
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,1 +1,22 @@
-def plot_accuracy_plot():
+def plot_accuracy_plot(model= ):
+    # Summarize history for accuracy
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(model.history['accuracy'])
+    plt.plot(model.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+
+    # Summarize history for loss
+    plt.subplot(1, 2, 2)
+    plt.plot(model.history['loss'])
+    plt.plot(model.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
