88d62c7d9e6b4519fa196c8081752cef1c6a15e6
Testimony Adekoya <hardeykoryar7@gmail.com>
2023-11-09 13:47:04 +0100

    Parenthesis and other small PEP issues

--- src/test_model.py
+++ src/test_model.py
@@ -1,8 +1,7 @@
 # importing libraries
-import tensorflow as tf
 import numpy as np
-from tensorflow.keras import models
-from tensorflow.keras.models import load_model
+# from tensorflow.keras import models
+# from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from .models.get_model import download_model
@@ -14,7 +13,7 @@ img_ = '../data/external/cr_leaf.jpeg'
 # preprocessing external test image
 def model_test(img_path, model):
     img_path = img_path
-    img = image.load_img(img_path, target_size= (224,224))
+    img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
