e06c4cf2efafb354a400bf7509a215d40109526b
Testimony Adekoya <hardeykoryar7@gmail.com>
2023-11-13 10:10:22 +0100

    Still figuring out the efficientnet shit

--- src/train_model.py
+++ src/train_model.py
@@ -119,13 +119,22 @@ model.save("model_vgg.h5", save_format="h5")
 # Removing Inception model from code
 
 # EfficientNet
-inputs_1 = tf.keras.Input(shape=(260, 260, 3))
 mymodel = enet.EfficientNetB2(input_shape=(260, 260, 3), include_top=False, weights='imagenet')
+
+inputs_1 = tf.keras.Input(shape=(260, 260, 3))
 x = tf.keras.layers.AveragePooling2D(pool_size=(7, 7))(mymodel.output)
 x = tf.keras.layers.Flatten()(x)
 predictors = tf.keras.layers.Dense(4, activation='softmax', name='Predictions')(x)
 final_model = Model(mymodel.input, outputs=predictors)
 
+
+def model(new_model=final_model, layers_num=1, trainable=False):
+    for layer in new_model.layers[:layers_num]:
+        layer.trainable = trainable
+    return new_model
+
+
+final_model = model(final_model)
 
 final_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
                     loss="categorical_crossentropy",
